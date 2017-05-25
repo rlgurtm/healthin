95119ddaaa46a2ea2b96659159077b0c9cd02248
KOSTA <KOSTA@192.168.0.151>
2017-05-25 13:14:00 +0900

    gitignore

--- healthin/src/test/resources/health.sql
+++ healthin/src/test/resources/health.sql
@@ -1,1 +1,13 @@
- 
+ create table member(
+	id varchar2(100) primary key,
+	password varchar2(100) not null,
+	nickname varchar2(100) not null,
+	name varchar2(100) not null,
+	birthdate varchar2(100) not null, 
+	gender varchar2(100) not null,
+	address varchar2(100) not null,
+	tel varchar2(100) not null,
+	e_mail varchar2(100) not null,
+	is_trainer varchar2(100) not null,
+	withdrawal char(1) default('N')
+)
